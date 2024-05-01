f9a0080153fecb99443ef3d6fdc8f9d759164a53
Starrleto <ellierasuli@gmail.com>
2024-05-01 12:44:30 -0700

    e

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 import NavbarComponent from "@/Components/NavbarComponent";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { AboutMe } from "@/Components/AboutMe";
 import { ArtGaller } from "@/Components/ArtGaller";
 import { Projects } from "@/Components/Projects";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
