6f0f2390d092d3dc32c3c27cfd28c7290361d348
Starrleto <ellierasuli@gmail.com>
2024-05-01 12:33:33 -0700

    h

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 export const Projects = () => {
   return (
-    <div>Projects</div>
+    <div>
+      
+    <Container>
+      <Row>
+        <Col>
+        
+        </Col>
+        <Col>
+          A Rock Paper Scissors (Lizard Spock) Game!
+        </Col>
+      </Row>
+    </Container>
+
+    </div>
   )
 }
